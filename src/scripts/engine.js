3af0cdb9f1946fdd79e90b991e2a49d4137fede0
szlima <suzanelima@outlook.com>
2024-01-15 11:50:20 -0300

    Tocando o áudio pelo teclado do computador

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,12 +1,24 @@
 const pianoKeys= document.querySelectorAll(".piano-keys .key");
-
+let mapedKeys= [];
 let audio= new Audio("src/tunes/a.wav");
 
 const playTune= key => {
     audio.src= `src/tunes/${key}.wav`;
     audio.play();
+
+    const clickedKey= document.querySelector(`[data-key="${key}"]`);
+    clickedKey.classList.add("active");
+    setTimeout(() =>
+        clickedKey.classList.remove("active")
+    , 150);
 };
 
-pianoKeys.forEach(key =>
-    key.addEventListener("click", () => playTune(key.dataset.key))
-);
+pianoKeys.forEach(pianoKey => {
+    pianoKey.addEventListener("click", () => playTune(pianoKey.dataset.key));
+    mapedKeys.push(pianoKey.dataset.key);
+});
+
+document.addEventListener("keydown", e => {
+    if(mapedKeys.includes(e.key))
+        playTune(e.key);
+});
