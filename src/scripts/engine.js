4f93e39aef22ec275851a80f6760eea70e18a20a
szlima <suzanelima@outlook.com>
2024-01-15 16:10:19 -0300

    Ativando/desativando áudio pelo teclado do computador

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,5 +1,6 @@
 const pianoKeys= document.querySelectorAll(".piano-keys .key");
 const volumeSlider= document.querySelector(".volume-slider input");
+const keysCheck= document.querySelector(".keys-check input");
 
 let mapedKeys= [];
 let audio= new Audio("src/tunes/a.wav");
@@ -21,7 +22,7 @@ pianoKeys.forEach(pianoKey => {
 });
 
 document.addEventListener("keydown", e => {
-    if(mapedKeys.includes(e.key))
+    if(keysCheck.checked && mapedKeys.includes(e.key))
         playTune(e.key);
 });
 
@@ -29,4 +30,12 @@ const handleVolume= e => {
     audio.volume= e.target.value;
 };
 
+const showHideKeys= (e) => {
+    pianoKeys.forEach(pianoKey => {
+        pianoKey.classList.toggle("hide");
+    });
+};
+
 volumeSlider.addEventListener("input", handleVolume);
+
+keysCheck.addEventListener("click", showHideKeys);
